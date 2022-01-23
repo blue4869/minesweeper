2d08a4d051bd683405bf1348f46390b73e857807
nakagawakenta <jyuhjgerftbk@yahoo.co.jp>
2022-01-23 22:00:32 +0900

    コメント追加

--- minesweeper/minesweeper/Constants.java
+++ minesweeper/minesweeper/Constants.java
@@ -1,8 +1,9 @@
 package swing.minesweeper;
 
 public class Constants {
+    /** 地雷判別用文字列 */
     public static final String MINE = "mine";
-    public static final String DISPLAY_MINE = "*";
-    public static final String NONE = "";
 
+    /** 画面表示用地雷 */
+    public static final String DISPLAY_MINE = "*";
 }
