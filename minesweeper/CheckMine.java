2eb77cf1bade9e1c95f6d5a1976d915da6879556
nakagawakenta <jyuhjgerftbk@yahoo.co.jp>
2025-10-08 21:20:50 +0900

    レビュー指摘対応

--- minesweeper/CheckMine.java
+++ minesweeper/CheckMine.java
@@ -1,4 +1,4 @@
-package swing.minesweeper;
+package minesweeper.minesweeper;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,6 +26,18 @@ public class CheckMine extends JFrame implements ActionListener {
 
 	/** すでに確認したマスかのフラグ */
 	public static final String CHECKED = "checked";
+
+	private static final String KEY_KIND = "kind";
+	private static final String KEY_STATE = "state";
+	private static final String KEY_COUNT = "count";
+
+  enum Kind {
+    MINE, NUMBER
+  }
+
+	enum State {
+		HIDDEN, REVEALED, FLAGGED
+	}
 
 	public CheckMine (JFrame frame, JPanel p, int[] data) {
 		this.frame = frame;
@@ -106,13 +118,16 @@ public class CheckMine extends JFrame implements ActionListener {
 
 		b.setFont(new Font("HGPｺﾞｼｯｸE", Font.BOLD, 20));
 
-		if (MINE.equals(b.getName())) {
+		Kind kind = (Kind) b.getClientProperty(KEY_KIND);
+		Integer count = (Integer) b.getClientProperty(KEY_COUNT);
+
+		if (kind == Kind.MINE) {
 			b.setText(Constants.DISPLAY_MINE);
 		} else {
-			b.setText(b.getName());
+			b.setText(String.valueOf(count));
 		}
 
-		b.setName(CHECKED);
+		b.putClientProperty(KEY_STATE, State.REVEALED);
 		b.setEnabled(false);
 	}
 
@@ -124,16 +139,15 @@ public class CheckMine extends JFrame implements ActionListener {
 	 * @param data ゲームの各データ
 	 */
 	public static void nameCheck(JButton button, JPanel p, int[] data) {
-		String buttonName = button.getName();
-		if (CHECKED.equals(buttonName)) {
+		State st = (State) button.getClientProperty(KEY_STATE);
+		if (st == State.REVEALED)
 			return;
-		}
 
-		if (ZERO.equals(buttonName)) {
+		Integer count = (Integer) button.getClientProperty(KEY_COUNT);
+		if (count != null && count == 0) {
 			openAllZero(button, p, data);
 		} else {
 			setMineText(button);
-			button.setEnabled(false);
 		}
 	}
 }
